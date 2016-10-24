7069364ce673ea0901bb9fb0023c830542c56cfb
Christian Hergert <chergert@redhat.com>
2016-10-24 01:49:55 -0700

    license: LGPL-2.1 is good enough for this project

--- aws-glib/aws-s3-client.c
+++ aws-glib/aws-s3-client.c
@@ -1,19 +1,19 @@
-/* aws-s3-client.c
+/* aws-s3-client.h
  *
- * Copyright (C) 2012 Christian Hergert <chris@dronelabs.com>
+ * Copyright © 2012-2016 Christian Hergert <christian@hergert.me>
  *
- * This file is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This file is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- * This file is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This file is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <glib/gi18n.h>
